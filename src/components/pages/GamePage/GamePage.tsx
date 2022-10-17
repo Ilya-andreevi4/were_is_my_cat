6c2ce19c4c7f1c79e1d61c3ac1e38b96f785de32
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-17 12:50:31 +0600

    изменено:      src/components/pages/GamePage/GamePage.tsx 	изменено:      src/index.css

--- src/components/pages/GamePage/GamePage.tsx
+++ src/components/pages/GamePage/GamePage.tsx
@@ -1,7 +1,10 @@
 function GamePage() {
 
   return (
-    <div className="GamePage">
+    <div className="flex flex-col justify-between left-0 mx-auto my-auto top-[5%] h-[50%]">
+      <div className="flex mx-auto mt-4 bg-orange-500 w-96 h-20 rounded-xl fancy-border">
+
+      </div>
 
     </div>
   )
