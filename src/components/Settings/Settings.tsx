af26c2982850378dd4d87f4e9b9ca2185164d486
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-19 17:57:50 +0600

    изменено:      src/components/Settings/Settings.tsx

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -17,7 +17,7 @@ const Settings: FC<SettingsProps> = ({ active, setActive, children }) => {
       onClick={() => setActive(false)}
     >
       <div
-        className="grid grid-cols-1 gap-10 mt-[5%] text-blue-700 text-3xl leading-loose justify-between font-sans font-bold text-center px-10 py-4 border-blue-700 border-4 rounded-2xl bg-white w-fit h-fit mx-auto my-auto transition-opacity-1 opacity-100"
+        className="grid grid-cols-1 gap-10 mt-4 text-blue-700 text-3xl leading-loose justify-between font-sans font-bold text-center px-10 py-4 border-blue-700 border-4 rounded-2xl bg-white w-fit h-fit mx-auto my-auto transition-opacity-1 opacity-100"
         onClick={(e) => e.stopPropagation()}
       >
         {children}
