0c9c6d8a3776c590d22a82c31a0ed3a2b547e7e1
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-20 13:02:59 +0600

    +css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,8 +13,8 @@ export default function Header() {
   };
 
   return (
-    <nav className="flex justify-between items-center h-16 w-full py-2 px-5 shadow-lg bg-gradient-to-tr from-blue-500 to-blue-600 bg-blue-500 text-white ">
-      <img src="./axi_logo.png" alt="Axioma" className="mx-2 h-12 w-12 z-10" />
+    <nav className="flex justify-between items-center h-12 w-full py-2 px-5 shadow-lg bg-gradient-to-tr from-blue-500 to-blue-600 bg-blue-500 text-white ">
+      <img src="./axi_logo.png" alt="Axioma" className="mx-2 h-10 w-10 z-10" />
 
       <Settings active={modalActive} setActive={setModalActive}>
         <h1>Настройки</h1>
@@ -34,7 +34,7 @@ export default function Header() {
       </Settings>
 
       <button
-        className="h-12 w-12 p-1 bg-orange-600 border-slate-100 border-2 shadow-black shadow-3xl z-10 hover:shadow-sm hover:scale-105 rounded-2xl inline-block"
+        className="h-10 w-10 p-1 bg-orange-600 border-slate-100 border-2 shadow-black shadow-3xl z-10 hover:shadow-sm hover:scale-105 rounded-2xl inline-block"
         onClick={() => setModalActive(true)}
       >
         <img src="./setting.svg" alt="Settings" className="block z-10" />
--- src/components/pages/GamePage/GamePage.tsx
+++ src/components/pages/GamePage/GamePage.tsx
@@ -20,12 +20,12 @@ function GamePage() {
   return (
     <div className="bg-gradient-to-tr from-amber-200 to-orange-200 bg-orange-200 h-fit w-screen min-h-screen">
     <div className="flex flex-col justify-between left-0 mx-auto my-auto top-[5%] h-[50%]">
-      <div className="flex text-white justify-center text-center pb-1 px-2 font-sans text-2xl font-extrabold mx-auto my-2 bg-gradient-to-tr from-blue-400 to-blue-400 bg-cyan-600 w-fit h-16 fancy-border transition ease-in-out delay-1500 shadow-md hover:shadow-sm ">
+      <div className="flex text-white justify-center text-center pb-1 px-2 font-sans text-xl font-extrabold mx-auto mt-4 bg-gradient-to-tr from-orange-400 to-amber-500 bg-orange-400 w-fit h-10 fancy-border transition ease-in-out delay-1500 shadow-md hover:shadow-sm ">
         <h2 className="my-auto">{activePlayer?`Ходит ${activePlayer?.name}`:"Найди кота!"}</h2>
       </div>
       <div className="flex flex-row">
       {/* bg-gradient-to-br from-amber-600 to-orange-500 rounded-xl */}
-        <div className="grid grid-cols-3 md:grid-cols-7 lg:grid-cols-10 gap-4 p-2 mx-1 mb-8 md:p-6 md:mx-6 justify-between items-baseline h-fit w-[80%] ">
+        <div className="grid grid-cols-3 md:grid-cols-7 lg:grid-cols-10 gap-4 p-2 mx-1 md:p-6 md:mx-6 justify-between items-baseline h-fit w-[80%] ">
           {cards.map((card) => {
             return (
               <button
