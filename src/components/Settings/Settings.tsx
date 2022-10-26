958be73dae2fb230bf7789a23f821829aee022eb
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-26 18:03:00 +0600

    изменено:      src/components/Settings/Settings.tsx

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -17,7 +17,7 @@ const Settings: FC<SettingsProps> = ({ active, setActive, children }) => {
       onClick={() => setActive(false)}
     >
       <div
-        className="grid grid-cols-1 gap-1 mt-5 z-50 text-blue-700 text-xl leading-loose overflow-x-scroll max-h-[80%] justify-items-end font-sans font-bold text-center px-6 py-3 border-blue-700 border-2 rounded-xl bg-white w-fit h-fit mx-auto my-auto transition-opacity-1 opacity-100"
+        className="grid grid-cols-1 gap-1 mt-5 z-50 text-blue-700 text-xl leading-loose overflow-auto overflow-y-scroll max-h-[80%] justify-items-end font-sans font-bold text-center px-6 py-3 border-blue-700 border-2 rounded-xl bg-white w-fit h-fit mx-auto my-auto transition-opacity-1 opacity-100"
         onClick={(e) => e.stopPropagation()}
       >
         <button
